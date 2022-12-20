8ee6287cb6b91e4153c9c415eb3a049c8eca4ced
sachinjadav02 <121082965+sachinjadav02@users.noreply.github.com>
2022-12-20 12:06:20 +0530

    Add files via upload

---
+++ Untitled-1.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+ printf("#\n##\n###\n####\n");
+ printf("#########\n#\t#\n#\t#\n#\t#\n#\t#\n#########\n");	
+ printf("**\t**\n*******\n***\t********\n*\t******\n*****\n**\n*\n*");	
+ return 0;	
+	
+}
